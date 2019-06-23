292cc133bc7a6639bb082e9c57861995ddaf48bf
kwajo <kwajom@gmail.com>
2019-06-23 07:33:00 -0400

    cleaned up client error messages

--- one/client.py
+++ one/client.py
@@ -11,42 +11,39 @@ class Client:
         self.socket = None
         self.BUFFSIZE = 2048
         self.msg_buffer = ''
+        self.local_port = ()
     def create_socket(self):
-        print("creating socket")
-        '''
-        define the ipv4 address to bind to
-        INADDR_ANY will bind to all interfaces
-
-        https://stackoverflow.com/questions/16508685/understanding-inaddr-any-for-socket-programming what is inaddr
-        https://realpython.com/python-sockets/ diagram for creating a chat 
-        '''
+        print("Creating socket")
         
-
         #to bind the socket object to an ipv4 port we need to pass a host, port tuple
         
 
         #use with to ensure connection is closed
         try:
             self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            print("socket Created")
+            self.local_port = self.socket.getsockname()
+            print('Socket Created')
         except socket.error as e:
             print (e)
 
 
     def connect_to_server(self):
-        print("Connecting to server")
+        print('Connecting to QwackChat')
+        print('Host address-->     {}:{}'.format(c.HOST_IP,c.HOST_PORT))
+        print('Local address-->    {}:{}'.format(self.local_port[0],self.local_port[1]))
         try:
             self.socket.connect(self.IPV4_ADDRESS)
-            print("Connected to server Created")
-        except socket.error as e:
-            print (e)
-
-
+            print("Connected")
+        except:
+            print ('Connection to-->    {}:{} failed\nExiting'.format(self.HOST_IP,self.HOST_PORT))
+            sys.exit()
 
     def send_message(self, message):
         msg = bytes(message,'UTF-8')
-        self.socket.send(msg)
-        print('message delivered')
+        try:
+            self.socket.send(msg)
+        except socket.error as e:
+            print(e)
     
     def recieve_message(self):
         data = self.socket.recv(self.BUFFSIZE)
@@ -54,11 +51,11 @@ class Client:
         
 
 c = Client()
-print(c.socket)
 c.create_socket()
-print(c.socket)
 c.connect_to_server()
-print(c.socket)
+
+
+
 while True:
     
     
